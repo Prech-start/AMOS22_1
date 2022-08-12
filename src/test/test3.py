2efb90c94da0e685662c9ef21d7c451fd59c9c5d
Prech-start <60168431+Prech-start@users.noreply.github.com>
2022-08-12 21:08:24 +0800

    src

--- src/test/test3.py
+++ src/test/test3.py
@@ -1,7 +1,3 @@
-# if __name__ == '__main__':
-#     for i in range(16):
-#         print(i)
+from src.utils.train_utils import *
 
-import easyocr
-reader = easyocr.Reader(['ch_sim','en']) # 只需要运行一次就可以将模型加载到内存中
-result = reader.readtext('chinese.jpg')
+pic_loss_acc()
--- src/test/test6.py
+++ src/test/test6.py
@@ -1,3 +1,48 @@
-from src.utils.train_utils import *
-pic_loss_acc()
-print(0)
+import numpy as np
+import os
+import json
+import SimpleITK as sitk
+import pandas as pd
+
+
+def get_info_():
+    json_path = os.path.join('..', '..', 'data', 'AMOS22', 'task1_dataset.json')
+    task_json = json.load(open(json_path))
+    train_paths = task_json['training']
+    dicts = [
+        'image_name',
+        'image_min',
+        'image_max',
+        'image_mean',
+        'image_median',
+        'image_size',
+        'label_name',
+        'label_num_class'
+    ]
+    info_xlsx = []
+    for image_label in train_paths:
+        image_path = image_label['image']
+        label_path = image_label['label']
+        image_array = load_(image_path)
+        label_array = load_(label_path)
+        image_min = np.min(image_array)
+        image_max = np.max(image_array)
+        image_mean = np.mean(image_array)
+        image_median = np.median(image_array)
+        image_size = image_array.shape
+        label_num_class = np.unique(label_array)
+        info_xlsx.append(
+            [image_path, image_min, image_max, image_mean
+                , image_median, image_size, label_path, label_num_class])
+    info_xlsx = np.array(info_xlsx)
+    info_xlsx = pd.DataFrame(info_xlsx)
+    info_xlsx.columns = dicts
+    info_xlsx.to_csv(os.path.join('..', 'output', 'info_task1.csv'))
+
+
+def load_(path):
+    image_ = sitk.ReadImage(os.path.join('..', '..', 'data', 'AMOS22', path))
+    return np.array(sitk.GetArrayFromImage(image_)).astype(np.float32)
+
+
+get_info_()
