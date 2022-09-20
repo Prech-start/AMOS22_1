72981c2c83fe0a6c4d3e7475f66814db463855db
LuoJC <60168431+Prech-start@users.noreply.github.com>
2022-09-20 20:08:00 +0800

    计算dice

--- src/model/model.py
+++ src/model/model.py
@@ -7,85 +7,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-class UNet(nn.Module):
-    def __init__(self, in_channel=1, out_channel=2, training=True):
-        super(UNet, self).__init__()
-        self.training = training
-        self.encoder1 = nn.Conv3d(in_channel, 32, 3, stride=1, padding=1)  # b, 16, 10, 10
-        self.encoder2 = nn.Conv3d(32, 64, 3, stride=1, padding=1)  # b, 8, 3, 3
-        self.encoder3 = nn.Conv3d(64, 128, 3, stride=1, padding=1)
-        self.encoder4 = nn.Conv3d(128, 256, 3, stride=1, padding=1)
-        # self.encoder5=   nn.Conv3d(256, 512, 3, stride=1, padding=1)
-
-        # self.decoder1 = nn.Conv3d(512, 256, 3, stride=1,padding=1)  # b, 16, 5, 5
-        self.decoder2 = nn.Conv3d(256, 128, 3, stride=1, padding=1)  # b, 8, 15, 1
-        self.decoder3 = nn.Conv3d(128, 64, 3, stride=1, padding=1)  # b, 1, 28, 28
-        self.decoder4 = nn.Conv3d(64, 32, 3, stride=1, padding=1)
-        self.decoder5 = nn.Conv3d(32, 2, 3, stride=1, padding=1)
-
-        self.map4 = nn.Sequential(
-            nn.Conv3d(2, out_channel, 1, 1),
-            nn.Upsample(scale_factor=(1, 2, 2), mode='trilinear'),
-            nn.Softmax(dim=1)
-        )
-
-        # 128*128 尺度下的映射
-        self.map3 = nn.Sequential(
-            nn.Conv3d(64, out_channel, 1, 1),
-            nn.Upsample(scale_factor=(4, 8, 8), mode='trilinear'),
-            nn.Softmax(dim=1)
-        )
-
-        # 64*64 尺度下的映射
-        self.map2 = nn.Sequential(
-            nn.Conv3d(128, out_channel, 1, 1),
-            nn.Upsample(scale_factor=(8, 16, 16), mode='trilinear'),
-            nn.Softmax(dim=1)
-        )
-
-        # 32*32 尺度下的映射
-        self.map1 = nn.Sequential(
-            nn.Conv3d(256, out_channel, 1, 1),
-            nn.Upsample(scale_factor=(16, 32, 32), mode='trilinear'),
-            nn.Softmax(dim=1)
-        )
-
-    def forward(self, x):
-
-        out = F.relu(F.max_pool3d(self.encoder1(x), 2, 2))
-        t1 = out
-        out = F.relu(F.max_pool3d(self.encoder2(out), 2, 2))
-        t2 = out
-        out = F.relu(F.max_pool3d(self.encoder3(out), 2, 2))
-        t3 = out
-        out = F.relu(F.max_pool3d(self.encoder4(out), 2, 2))
-        # t4 = out
-        # out = F.relu(F.max_pool3d(self.encoder5(out),2,2))
-
-        # t2 = out
-        # out = F.relu(F.interpolate(self.decoder1(out),scale_factor=(2,2,2),mode ='trilinear'))
-        # print(out.shape,t4.shape)
-        output1 = self.map1(out)
-        out = F.relu(F.interpolate(self.decoder2(out), scale_factor=(2, 2, 2), mode='trilinear'))
-        out = torch.add(out, t3)
-        output2 = self.map2(out)
-        out = F.relu(F.interpolate(self.decoder3(out), scale_factor=(2, 2, 2), mode='trilinear'))
-        out = torch.add(out, t2)
-        output3 = self.map3(out)
-        out = F.relu(F.interpolate(self.decoder4(out), scale_factor=(2, 2, 2), mode='trilinear'))
-        out = torch.add(out, t1)
-
-        out = F.relu(F.interpolate(self.decoder5(out), scale_factor=(2, 2, 2), mode='trilinear'))
-        output4 = self.map4(out)
-        # print(out.shape)
-        # print(output1.shape,output2.shape,output3.shape,output4.shape)
-        if self.training is True:
-            return output1, output2, output3, output4
-        else:
-            return output4
-
-
 '''
     输入的数据为 B * C * W * H * L
 
@@ -324,17 +245,22 @@ class UnetModel(nn.Module):
         super(UnetModel, self).__init__()
         self.encoder = EncoderBlock(in_channels=in_channels, model_depth=model_depth)
         self.decoder = DecoderBlock(out_channels=out_channels, model_depth=model_depth)
-        if final_activation == "sigmoid":
-            self.sigmoid = nn.Sigmoid()
-        else:
-            self.sigmoid = nn.Softmax(dim=1)
+        self.decoder_centre = DecoderBlock(out_channels=1, model_depth=model_depth)
+        self.softmax = nn.Softmax(dim=1)
+        self.sigmoid = nn.Sigmoid()
+        # if final_activation == "sigmoid":
+        #     self.sigmoid = nn.Sigmoid()
+        # else:
+        #     self.sigmoid = nn.Softmax(dim=1)
 
     def forward(self, x):
         x, downsampling_features = self.encoder(x)
-        x = self.decoder(x, downsampling_features)
-        x = self.sigmoid(x)
+        seg_x = self.decoder(x, downsampling_features)
+        centre_x = self.decoder_centre(x, downsampling_features)
+        seg_x = self.softmax(seg_x)
+        centre_x = self.sigmoid(centre_x)
         # print("Final output shape: ", x.shape)
-        return x
+        return seg_x, centre_x
 
 
 class ConvTranspose(nn.Module):
@@ -356,5 +282,5 @@ if __name__ == '__main__':
     # skip = torch.rand(size=[1, 256, 14, 14, 1])
     model = UnetModel(1, 16, model_depth=4)
     # button shape 256,13,13,1
-    y = model(x)
-    print(y.shape)
+    y, z = model(x)
+    print(y.shape, z.shape)
