1d53885d1f68d746fa2824a65772549a24a51889
ljc <1542826909@qq.com>
2022-11-05 09:19:27 +0800

    src

--- src/train/train_centre_test.py
+++ src/train/train_centre_test.py
@@ -1,37 +1,42 @@
-import sys
-
-import torch
+from src.model.model_test import *
 from einops import *
 from src.utils.train_utils import *
 from src.train.loss import *
 
 sys.path.append('..')
 from src.utils.accuracy import *
-import src.process.task2_data_loader as loader
-from src.model.model_test import *
+import src.process.task2_data_loader_centre as loader
 
 
 def train_and_valid_model(epoch, model, data_loader, device, optimizer, criterion):
+    n_epoch, max_epoch = epoch
     # ---------------------------------------------------
     t_loss = []
     v_loss = []
     v_acc = []
     train_loader, valid_loader = data_loader
+    loss_dice, loss_L1 = criterion
+    weight = ((n_epoch - max_epoch) ** 2) / ((1 - max_epoch) ** 2)
     # ---------------------------------------------------
     model.train()
     model.to(device)
-    criterion.to(device)
-    for index, (data, y) in enumerate(train_loader):
+    for index, (data, GT, GT_centre) in enumerate(train_loader):
         # train_data
         optimizer.zero_grad()
-        # trans y to onehot
-        y = torch.LongTensor(y.long())
-        data, y = data.float().to(device), y.to(device)
-        y = one_hot(y, 16)
-        target = rearrange(y, 'b d w h c -> b c d w h')
+        # trans GT to onehot
+        GT = torch.LongTensor(GT.long())
+        data = data.float().to(device)
+        GT = one_hot(GT, 16)
+        GT = rearrange(GT, 'b d w h c -> b c d w h')
         # training param
-        output = model(Variable(data))
-        loss = criterion(output, target.float())
+        output, out_centre = model(Variable(data))
+        GT = GT.to(device)
+        loss_ = loss_dice(output, GT.float())
+        GT_centre = GT_centre.unsqueeze(0)
+        GT_centre = GT_centre.to(device)
+        # print(out_centre.shape, GT_centre.shape)
+        loss_centre = loss_L1(out_centre, GT_centre.float())
+        loss = (1 - weight) * loss_ + weight * loss_centre
         loss.backward()
         optimizer.step()
         t_loss.append(loss.item())
@@ -39,15 +44,15 @@ def train_and_valid_model(epoch, model, data_loader, device, optimizer, criterio
     print()
     model.eval()
     model.cpu()
-    criterion.cpu()
-    for index, (data, y) in enumerate(valid_loader):
+    for index, (data, GT, GT_centre) in enumerate(valid_loader):
         # valid data
-        y = torch.LongTensor(y.long())
-        y = one_hot(y, 16)
-        target = rearrange(y, 'b d w h c -> b c d w h')
+        GT = torch.LongTensor(GT.long())
+        GT = one_hot(GT, 16)
+
+        target = rearrange(GT, 'b d w h c -> b c d w h')
         # training param
-        output = model(data.float())
-        loss = criterion(output, target.float())
+        output, _ = model(data.float())
+        loss = loss_dice(output, target.float())
         v_acc.append(np.mean(
             calculate_acc(torch.argmax(output, dim=1), torch.argmax(target, dim=1), class_num=16, fun=DICE,
                           is_training=True)))
@@ -70,9 +75,6 @@ def train(pre_train_model, n_epochs, batch_size, optimizer, criterion, device, i
     if is_load:
         with open(os.path.join('{}.tmp'.format(strategy)), 'rb+') as f:
             train_loss, valid_loss, valid_acc = pickle.load(f)
-            train_loss = train_loss.tolist()
-            valid_loss = valid_loss.tolist()
-            valid_acc = valid_acc.tolist()
             max_acc = 0.5
     else:
         train_loss = []
@@ -107,27 +109,23 @@ def train(pre_train_model, n_epochs, batch_size, optimizer, criterion, device, i
 def run():
     print('beginning training')
     class_num = 16
-    learning_rate = 6e-4
-    epoch = 300
-    device = torch.device('cuda:0')
-    strategy = 'combo'
-    model = UnetModel(1, class_num, 6)
+    learning_rate = 1e-4
+    max_epoch = 300
+    model = UnetModel_centre(1, class_num, 6)
+    strategy = 'centre_test'
     # 是否加载模型
-    is_load = True
+    is_load = False
     # 是否迁移模型
     is_move = False
     if is_load:
-        model.load_state_dict(
-            torch.load(os.path.join(os.path.dirname(__file__), '..', 'checkpoints', strategy, 'Unet-new.pth')))
+        model.load_state_dict(torch.load(os.path.join('..', 'checkpoints', strategy, 'Unet-180.pth')))
     if is_move:
-        model.load_state_dict(torch.load(os.path.join('..', 'checkpoints', strategy, 'Unet-final.pth')))
+        model.load_state_dict(torch.load(os.path.join('..', 'checkpoints', strategy, 'Unet-210.pth')))
     loss_weight = [1, 2, 2, 3, 6, 6, 1, 4, 3, 4, 7, 8, 10, 5, 4, 5]
-    loss = ComboLoss2(loss_weight)
+    loss = BCELoss_with_weight(loss_weight)
+    loss_centre = torch.nn.L1Loss()
+    # 150 768min
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    # norm2 = 2028min
-    model = train(pre_train_model=model, n_epochs=epoch, batch_size=1, optimizer=optimizer, criterion=loss,
-                  device=torch.device('cuda:0'), is_load=is_load, strategy=strategy)
-
-
-if __name__ == '__main__':
-    run()
+    model = train(pre_train_model=model, n_epochs=max_epoch, batch_size=1, optimizer=optimizer,
+                  criterion=[loss, loss_centre],
+                  device=torch.device('cpu'), is_load=is_load, strategy=strategy)
