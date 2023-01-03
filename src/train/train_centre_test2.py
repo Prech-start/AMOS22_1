7635f4cb9756fe86083a764eaf6f2804c5f1fca1
wzz <15036281668@163.com>
2023-01-03 21:03:14 +0800

    sm

--- src/train/train_centre_test2.py
+++ src/train/train_centre_test2.py
@@ -111,7 +111,7 @@ def run():
     learning_rate = 1e-4
     max_epoch = 300
     model = UnetModel_centre2(1, class_num, 6)
-    strategy = 'centre_test2'
+    strategy = 'centre_test2_1e-4'
     # 是否加载模型
     is_load = False
     # 是否迁移模型
