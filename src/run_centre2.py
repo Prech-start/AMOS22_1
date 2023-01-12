7f52fce969139ce672f6e890c86212e3b1a1022c
LuoJC <60168431+Prech-start@users.noreply.github.com>
2023-01-12 22:23:55 +0800

    dice and taining and dataset generate

--- src/run_centre2.py
+++ src/run_centre2.py
@@ -43,42 +43,38 @@ valid_path = CT_valid_path  # + MRI_valid_path
 test_path = CT_test_path  # + MRI_test_path
 
 
-def cal_centre_point_4(label_arr: numpy.ndarray, label_file_path: str, r: int = 10, small_organ: list = None,
+def cal_centre_point_5(label_arr: numpy.ndarray, label_file_path: str, r: int = 10, small_organ: list = None,
                        spacing=None):
     start_time = time.time()
     file_name = label_file_path.split('/')[-1].split('.')[0]
     path_dir = os.path.dirname(__file__)
-    file_path = os.path.join(path_dir, '..', 'data', 'pointcloud6')
+    file_path = os.path.join(path_dir, '..', 'data', 'pointcloud7')
     if not os.path.exists(file_path):
         os.makedirs(file_path)
     save_path = os.path.join(file_path, '{}.npy'.format(file_name))
-    label_arr_ = torch.nn.functional.one_hot(torch.LongTensor(label_arr), 16).permute(-1, 0, 1, 2)
+    target = np.zeros([1, 3])
     if not os.path.exists(save_path):
-        centre_arr = np.zeros_like(label_arr_).astype(np.float32)
+        centre_arr = np.zeros_like(label_arr).astype(np.float32)
         for i in np.unique(label_arr).astype(int).tolist():
             if i == 0:
                 continue
             # 对于不同大小的器官，r的取值也不一样
-            # if i in small organ_list
-            # r = 2/3 max_distance_from_pixel_to_centre
-            class_xyz = np.array(np.where(label_arr_[i, ...] == 1))
-            class_all = np.array(np.where(label_arr_[i, ...] != -1))
-            class_xyz = np.r_[np.ones((1, class_xyz.shape[-1])) * i, class_xyz].astype(int)
-            class_all = np.r_[np.ones((1, class_all.shape[-1])) * i, class_all].astype(int)
-            c, x, y, z = np.mean(class_xyz, 1, dtype=int)
+            class_xyz = np.array(np.where(label_arr == i))
+            class_all = np.array(np.where(label_arr != -1))
             # 求中心点坐标
-            centre_arr[i, x, y, z] = 1
+            x, y, z = np.mean(class_xyz, 1, dtype=int)
             # 求每个label点到中心的点的距离
-            distance_for_xyz = np.linalg.norm(class_xyz.T - [i, x, y, z], axis=1)  # N * 1
+            distance_for_xyz = np.linalg.norm(class_xyz.T - [x, y, z], axis=1)  # N * 1
             if i in small_organ:
-                r = 2 / 3 * np.max(distance_for_xyz)
-            distance_for_xyz = np.linalg.norm(class_all.T - [i, x, y, z], axis=1)  # N * 1
+                r = 1 / 3 * np.max(distance_for_xyz)
+            distance_for_xyz = np.linalg.norm(class_all.T - [x, y, z], axis=1)  # N * 1
             # 筛选出distance中距离小于r的所有坐标 type:tuple
             dis = np.where(distance_for_xyz < r)
-            # 筛选出到中心点距离小于r的所有坐标
-            distance_list = distance_for_xyz[dis]
-            # 将每个筛选出的点进行赋值
-            centre_arr[tuple(class_all.T[dis].T.tolist())] = (r - distance_list) / r
+            # 求出所有点坐标
+            points = class_all.T[dis]
+            labels = i * np.ones((1, 3))
+            target = np.concatenate((target, labels), axis=0)
+            target = np.concatenate((target, points), axis=0)
             # if i in small_organ:
             #     # 第i个通道的数据取出来做spacing
             #     array = centre_arr[i, ...]
@@ -88,9 +84,11 @@ def cal_centre_point_4(label_arr: numpy.ndarray, label_file_path: str, r: int = 
             #     filename = 'test.nii.gz'
             #     sitk.WriteImage(IMAGE, filename)
             #     pass
-        np.save(save_path, centre_arr)
+        # print(target)
+        # pass
+        np.save(save_path, target)
         print('data {} processing is finished, it cost {}(s)'.format(file_name, time.time() - start_time))
-        return centre_arr
+        return target
     # print('data {} processing is finished, it cost {}(s)'.format(file_name, time.time() - start_time))
     return np.load(save_path).astype(np.int8)
 
@@ -111,7 +109,7 @@ class data_set(Dataset):
         # x = self.Standardization(x)
         # z = resize(z, (64, 256, 256), order=0, preserve_range=True, anti_aliasing=False)
         small_organ = [4, 5, 10, 11, 12, 13, 14, 15]
-        z = cal_centre_point_4(y.squeeze(), path_[item][1], r=10, small_organ=small_organ)
+        z = cal_centre_point_5(y.squeeze(), path_[item][1], r=10, small_organ=small_organ)
         x = torch.from_numpy(x).type(torch.FloatTensor).unsqueeze_(0)
         y = torch.from_numpy(y).type(torch.FloatTensor)
         z = torch.from_numpy(z).type(torch.FloatTensor)
@@ -550,18 +548,18 @@ if __name__ == '__main__':
             optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
         for index, (data, GT, C_GT) in enumerate(train_loader):
             save_path = os.path.join('output', 'AMOS_CENTRE', str(index))
-            for i in range(data.squeeze().shape[0]):
-                if not os.path.exists(save_path):
-                    os.makedirs(save_path)
-                plt.imsave(os.path.join(save_path, str(i) + '.png'), data.squeeze()[i, ...], cmap='gray')
-                GT[GT != 4] = 0
-                plt.imsave(os.path.join(save_path, str(i) + '_GT.png'), GT.squeeze()[i, ...], cmap='gray')
-                for j in range(C_GT.squeeze().shape[0]):
-                    save_path_ = os.path.join(save_path, str(j))
-                    if not os.path.exists(save_path_):
-                        os.makedirs(save_path_)
-                    plt.imsave(os.path.join(save_path_, str(i) + '_CGT.png'), C_GT.squeeze()[j, i, ...], cmap='gray')
-            print('finish')
+            # for i in range(data.squeeze().shape[0]):
+            #     if not os.path.exists(save_path):
+            #         os.makedirs(save_path)
+            #     plt.imsave(os.path.join(save_path, str(i) + '.png'), data.squeeze()[i, ...], cmap='gray')
+            #     GT[GT != 4] = 0
+            #     plt.imsave(os.path.join(save_path, str(i) + '_GT.png'), GT.squeeze()[i, ...], cmap='gray')
+            #     for j in range(C_GT.squeeze().shape[0]):
+            #         save_path_ = os.path.join(save_path, str(j))
+            #         if not os.path.exists(save_path_):
+            #             os.makedirs(save_path_)
+            #         plt.imsave(os.path.join(save_path_, str(i) + '_CGT.png'), C_GT.squeeze()[j, i, ...], cmap='gray')
+            # print('finish')
             pass
             continue
             # train_data
